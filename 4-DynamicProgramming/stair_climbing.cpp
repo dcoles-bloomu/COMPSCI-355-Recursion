a606042437b45c22e1fa511b6d5db5c0074e92da
Drue Coles <drue.coles@gmail.com>
2020-09-15 17:36:02 +0000

    Corrected base case in recursion.

--- 4-DynamicProgramming/stair_climbing.cpp
+++ 4-DynamicProgramming/stair_climbing.cpp
@@ -46,7 +46,7 @@ int main() {
 
 long rec_num_climbs(int num_steps) { 
     if (num_steps == 0 || num_steps == 1)
-        return num_steps + 1;   
+        return 1;   
     return rec_num_climbs(num_steps - 1) + rec_num_climbs(num_steps - 2);
 }
 
@@ -56,7 +56,7 @@ long dp_num_climbs(int num_steps) {
     }
     long a[num_steps + 1];
     a[0] = 1;
-    a[1] = 2;
+    a[1] = 1;
     for (int i = 2; i <= num_steps; i++) {
         a[i] = a[i - 1] + a[i - 2];
     }
